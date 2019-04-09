5b6372f536e1ad35950ddf322f293acbda3efce6
Vbobell <vinicius.bobel41@gmail.com>
2019-04-09 09:55:22 -0300

    Updates

---
+++ precache-manifest.630c15ff3b21d11b8e4397ce3341ecd2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3fe6eef4fe26cbfa36a",
+    "url": "/show-repository-commits/static/js/runtime~main.8b78d5e4.js"
+  },
+  {
+    "revision": "deb106d57a86a1eb55b3",
+    "url": "/show-repository-commits/static/js/main.14ccd0db.chunk.js"
+  },
+  {
+    "revision": "dfcd2bf44c23ceb6a29f",
+    "url": "/show-repository-commits/static/js/2.b61b7e1b.chunk.js"
+  },
+  {
+    "revision": "8dfea5df29abfc4fd28143caf0d39692",
+    "url": "/show-repository-commits/index.html"
+  }
+];

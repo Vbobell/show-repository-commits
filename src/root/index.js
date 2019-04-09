d9e154703392f945ac6fd72700d6ad5a99c337e3
Vbobell <vinicius.bobel41@gmail.com>
2019-04-09 10:03:53 -0300

    Change route

--- src/root/index.js
+++ src/root/index.js
@@ -8,7 +8,7 @@ const Root = () => {
     return (<Provider store={store}>
             <BrowserRouter>
             <Switch>
-                <Route path="/show-repository-commits/" component={AppSelect} />
+                <Route path="/" exact={true} component={AppSelect} />
             </Switch>
             </ BrowserRouter>
             </Provider>)

88309849eb20354b48bbae062e1b3628c9c57549
Vbobell <vinicius.bobel41@gmail.com>
2019-04-10 18:32:30 -0300

    Create path dist

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: __dirname + '/build',
+        path: __dirname + '/dist',
         publicPath: '/',
         filename: './bundle.js'
     },

29b9d86d13d1eba589e2e0c8640b290469d9c1e9
Vbobell <vinicius.bobel41@gmail.com>
2019-04-11 08:48:10 -0300

    Change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
     entry: path.join(__dirname, './src/index.js'),
     output: {
         path: path.join(__dirname, '/dist'),
-        publicPath: '/',
         filename: 'bundle.js'
     },
     devServer: {
